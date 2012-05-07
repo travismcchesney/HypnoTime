9cb369e18514958738d0ba6d004883e87f2131a7
Travis McChesney <travisj37@hotmail.com>
2012-05-06 21:41:11 -0600

    Initial Commit

---
+++ HypnoTime/HypnoAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  HypnoAppDelegate.h
+//  HypnoTime
+//
+//  Created by Travis McChesney on 5/6/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HypnoAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
