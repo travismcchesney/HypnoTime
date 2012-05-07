03c348049eb25ab705a4a6ed0963dc83b72ae7fc
Travis McChesney <travisj37@hotmail.com>
2012-05-06 21:46:25 -0600

    Initial commit

---
+++ HypnoTime/HypnosisViewController.h
@@ -0,0 +1,13 @@
+//
+//  HypnosisViewController.h
+//  HypnoTime
+//
+//  Created by Travis McChesney on 5/6/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface HypnosisViewController : NSObject
+
+@end
---
+++ HypnoTime/HypnosisViewController.m
@@ -0,0 +1,13 @@
+//
+//  HypnosisViewController.m
+//  HypnoTime
+//
+//  Created by Travis McChesney on 5/6/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "HypnosisViewController.h"
+
+@implementation HypnosisViewController
+
+@end
